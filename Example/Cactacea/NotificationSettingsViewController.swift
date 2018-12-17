bc7a1d06e5d81161d15cb932435659cc9ebe201b
TAKESHI SHIMADA <>
2018-12-17 21:27:03 +0900

    Initial Commit

---
+++ Example/Cactacea/NotificationSettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationSettingsViewController.swift
+//  Cactacea_Example
+//
+//  Created by TAKESHI SHIMADA on 2018/12/16.
+//  Copyright © 2018 Cactacea. All rights reserved.
+//
+
+import UIKit
+import Cactacea
+
+class NotificationSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        SettingsAPI.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
